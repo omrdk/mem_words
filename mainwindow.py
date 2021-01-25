b34d1c89b3a657196441dbecfc843a7e840a5511
omrdk <omrfrkdak@gmail.com>
2021-01-26 00:16:04 +0300

    v102

--- mainwindow.py
+++ mainwindow.py
@@ -9,8 +9,9 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore    import *
 from PyQt5 import QtTest
 from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QKeySequence
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QPushButton, QApplication, QLabel, QMainWindow, QShortcut, QAction, qApp, QMessageBox
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QMainWindow, QShortcut, QAction, qApp, QMessageBox
 
 file   = open('words.txt', 'r')         # *.txt file reading mode
 lines   = file.readlines()              # readlines
@@ -26,20 +27,22 @@ class mainwindow(QMainWindow):
         global cnt_t
         global cnt_f
         global selected_indexes
+        global btn_lst
+        global blink_v
         cnt_t = 0
         cnt_f = 3
-        selected_indexes = []                                 # indexes of selected lines
-        self.main_view()                                          # First screen
+        selected_indexes = []                                                   # indexes of selected lines
+        self.main_view()                                                        # First screen
 
         # Toolbar configuration
         self.toolBar = self.findChild(QToolBar, "toolbar")
         self.actionDictionary.triggered.connect(self.open_txt)
         self.actionDictionary.setIcon(QIcon("notebook--plus.png"))
         # Window configuration
-        self.setWindowTitle("Memmorize")
+        self.setWindowTitle("Memorizar")
         self.setFixedSize(400, 480)
         # Label definitions
-        self.lbl_1  = self.findChild(QLabel,     "lbl_1"    )
+        self.lbl_HP  = self.findChild(QLabel,     "lbl_HP"    )
         self.lbl_word  = self.findChild(QLabel,  "lbl_word" )
         self.lbl_count  = self.findChild(QLabel, "lbl_count")
         # Button definitions
@@ -52,20 +55,22 @@ class mainwindow(QMainWindow):
         self.btn_C   = self.findChild(QPushButton, "btn_C")
         self.btn_D   = self.findChild(QPushButton, "btn_D")
         # Shortcuts for buttons
-        self.btn_A.setShortcut( QKeySequence("1") )
-        self.btn_B.setShortcut( QKeySequence("2") )
-        self.btn_C.setShortcut( QKeySequence("3") )
-        self.btn_D.setShortcut( QKeySequence("4") )
+        #self.btn_A.setShortcut( QKeySequence("1") )
+        #self.btn_B.setShortcut( QKeySequence("2") )
+        #self.btn_C.setShortcut( QKeySequence("3") )
+        #self.btn_D.setShortcut( QKeySequence("4") )
 
+        #self.btn_A = QShortcut(QKeySequence('1'), self)
+        #self.btn_A.activated.connect(lambda: self.check_word(self.btn_A.()))
 
         self.btn_start.clicked.connect(self.back_count)                         # if btn_start is pressed then call fill_word function
-        self.btn_skip.clicked.connect(self.fill_word)                           # if btn_slip is pressed then call fill_word function
-        self.btn_exit.clicked.connect(self.main_view)                           # if btn_start is pressed then call main_page function
+        self.btn_skip.clicked.connect(self.fill_word)
+        self.btn_exit.clicked.connect(self.main_view)
         self.btn_rst.clicked.connect(self.rst_words)
         self.btn_A.clicked.connect(lambda: self.check_word(self.btn_A.text()))  # btn_A'nın text'ini check_word fonksiyonuna gönderir
         self.btn_B.clicked.connect(lambda: self.check_word(self.btn_B.text()))  # if btn_start is pressed then call main_page function
-        self.btn_C.clicked.connect(lambda: self.check_word(self.btn_C.text()))  # if btn_start is pressed then call main_page function
-        self.btn_D.clicked.connect(lambda: self.check_word(self.btn_D.text()))  # if btn_start is pressed then call main_page function
+        self.btn_C.clicked.connect(lambda: self.check_word(self.btn_C.text()))
+        self.btn_D.clicked.connect(lambda: self.check_word(self.btn_D.text()))
 
         self.show()
     # open words.txt
@@ -83,9 +88,13 @@ class mainwindow(QMainWindow):
 
     def main_view(self):
         # Start-up content and states
+        self.btn_A.setStyleSheet("background-color : rgb(48,48,48)")
+        self.btn_B.setStyleSheet("background-color : rgb(48,48,48)")
+        self.btn_C.setStyleSheet("background-color : rgb(48,48,48)")
+        self.btn_D.setStyleSheet("background-color : rgb(48,48,48)")
         self.lbl_word.setText("Press START")                                    # print en_word to lbl_word label
         self.lbl_count.setText(str(cnt_t))
-        self.lbl_1.setText(str(cnt_f))
+        self.lbl_HP.setText(str(cnt_f))
         self.btn_skip.setEnabled(False)
         self.btn_rst.setEnabled(False)
         self.btn_start.setEnabled(True)
@@ -99,7 +108,7 @@ class mainwindow(QMainWindow):
         for i in range(3,0,-1):
             if i > 0:
                 self.lbl_word.setText(str(i))
-                QtTest.QTest.qWait(100)                                        #  time.sleep() is freezing the GUI!!
+                QtTest.QTest.qWait(1000)                                         #  time.sleep() is freezing the GUI!!
         self.fill_word()
         # Init states for buttons
         self.btn_skip.setEnabled(True)                                          # skip button is checkable now
@@ -114,7 +123,7 @@ class mainwindow(QMainWindow):
         cnt_t = 0
         cnt_f = 3
         self.lbl_count.setText(str(cnt_t))
-        self.lbl_1.setText(str(cnt_f))
+        self.lbl_HP.setText(str(cnt_f))
 
     # Insert to 'r' a random line number from words.txt and assign the name one of our buttons then do this for every index
     def fill_word(self):
@@ -127,14 +136,14 @@ class mainwindow(QMainWindow):
             self.btn_C.setEnabled(False)
             self.btn_D.setEnabled(False)
             return
-
         choice_list = [0,1,2,3]
+        global btn_lst
         btn_lst = [self.btn_A, self.btn_B, self.btn_C, self.btn_D]
-        random_nums = []                                                                           # şıkların satır sayıları
+        random_nums = []                                                        # şıkların satır sayıları
         words = []
         for i in range(0,4,1):
-            random_num = random.randint(0,len(lines) - 1)                                          # sonuncuyu alıyor mu??
-            while ((random_num in random_nums) or (random_num in selected_indexes)):               # eğer olusturdugumuz random sayı random listte varsa veya onceden secılmısse tekrar random al
+            random_num = random.randint(0,len(lines) - 1)
+            while ((random_num in random_nums) or (random_num in selected_indexes)):               # gelen sayı random list'te varsa veya önceden seçilmişse tekrar random al
                 random_num = random.randint(0,len(lines) - 1)
             random_nums.append(random_num)
             random_word = lines[random_num]
@@ -148,9 +157,11 @@ class mainwindow(QMainWindow):
                 selected_indexes.append(random_num)
             j = random.choice(choice_list)
             btn_lst[j].setText(tr_word)
-            choice_list.remove(j)                                                                  # boş şık kalmasın
+            if i == 0:
+                global blink_v
+                blink_v = j
+            choice_list.remove(j)                                               # boş şık kalmasın
         #print(*random_nums)
-
 
     # Check the words, if they match than plus 1 true counter(cnt_t), else minus 1 false counter(cnt_f)
     def check_word(self, btn_word):
@@ -161,20 +172,37 @@ class mainwindow(QMainWindow):
             self.lbl_count.setText(str(cnt_t))
             self.fill_word()
         else:
+            self.wrong_blink()
             global cnt_f
             cnt_f-=1
-            self.lbl_1.setText(str(cnt_f))
+            self.lbl_HP.setText(str(cnt_f))
             if cnt_f == 0:
                 self.main_view()
                 return
             self.fill_word()
 
+    # btn_rst basıldığında tekrarlanan sayıları tekrar listeye ekler
     def rst_words(self):
         global selected_indexes
-        selected_indexes = []                       # btn_rst basıldığında tekrarlanan sayıları tekrar listeye ekler
+        selected_indexes = []
+
+    def wrong_blink(self):
+        self.lbl_HP.setStyleSheet("background-color : rgb(198,0,0)")
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(198,0,0)")
+        QtTest.QTest.qWait(150)
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(48,48,48)")
+        QtTest.QTest.qWait(150)
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(198,0,0)")
+        QtTest.QTest.qWait(150)
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(48,48,48)")
+        QtTest.QTest.qWait(150)
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(198,0,0)")
+        QtTest.QTest.qWait(150)
+        btn_lst[blink_v].setStyleSheet("background-color : rgb(48,48,48)")
+        self.lbl_HP.setStyleSheet("background-color : #1155AD")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = mainwindow()
-#    window.show()
+#   window.show()
     sys.exit(app.exec_())
